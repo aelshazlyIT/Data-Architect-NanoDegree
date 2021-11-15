b73e303fd67e23c51f019aa581a5f7b86d8a66a0
Alaa Elshazly <77097437+aelshazlyIT@users.noreply.github.com>
2021-11-15 12:41:10 +0300

    Add files via upload

---
+++ Human Resources Database Design/DDL_SQL_Commands.sql
@@ -0,0 +1,59 @@
+--  Drop Tables if Exists
+DROP TABLE IF EXISTS JobMapping;
+DROP TABLE IF EXISTS OfficeLocation;
+DROP TABLE IF EXISTS Department;
+DROP TABLE IF EXISTS Salary;
+DROP TABLE IF EXISTS Job;
+DROP TABLE IF EXISTS Employee;
+DROP TABLE IF EXISTS Education;
+
+
+-- Create Tables
+
+CREATE TABLE Education(
+    education_id SERIAL PRIMARY KEY,
+    education_level VARCHAR(50)
+);
+
+CREATE TABLE Employee(
+    employee_id VARCHAR(128) PRIMARY KEY,
+    emp_name VARCHAR(128),
+    email VARCHAR(128),
+    education_id INT REFERENCES Education(education_id),
+    hiredate DATE
+);
+
+CREATE TABLE Job(
+    job_id SERIAL PRIMARY KEY,
+    jobtitle VARCHAR(128)
+);
+
+CREATE TABLE Salary(
+    salary_id SERIAL PRIMARY KEY,
+    salary int
+);
+
+CREATE TABLE Department(
+    department_id SERIAL PRIMARY KEY,
+    department VARCHAR(128)
+);
+
+CREATE TABLE OfficeLocation(
+    officelocation_id SERIAL PRIMARY KEY,
+    location_name VARCHAR(128),
+    location_address VARCHAR(128), 
+    city VARCHAR(128),
+    state VARCHAR(128)
+);
+
+CREATE TABLE JobMapping(
+    emp_id VARCHAR(128) REFERENCES Employee(employee_id),
+    job_id INT REFERENCES Job(job_id),
+    salary_id INT REFERENCES Salary(salary_id),
+    department_id INT REFERENCES Department(department_id),
+    officelocation_id INT REFERENCES OfficeLocation(officelocation_id),
+    start_date DATE,
+    end_date DATE,
+    manager_id VARCHAR(128) REFERENCES Employee(employee_id),
+    PRIMARY KEY (emp_id, job_id)
+);
---
+++ Human Resources Database Design/DML_SQL_Commands.sql
@@ -0,0 +1,46 @@
+
+-- Import data from staging
+ 
+--Import Data From Staging Table
+
+INSERT INTO Education(education_level)
+    SELECT DISTINCT education_lvl
+    FROM proj_stg;
+
+INSERT INTO Employee(employee_id, emp_name, email, education_id, hiredate)
+    SELECT DISTINCT s.Emp_ID, s.Emp_NM, s.Email, ed.education_id, s.hire_dt
+    FROM proj_stg as s
+    JOIN Education as ed
+    ON s.education_lvl = ed.education_level;
+
+INSERT INTO Job(jobtitle)
+    SELECT DISTINCT job_title
+    FROM proj_stg;
+
+INSERT INTO Department(department)
+    SELECT DISTINCT department_nm
+    FROM proj_stg;
+
+INSERT INTO OfficeLocation(location_name, location_address,
+        city, state)
+    SELECT DISTINCT location, address, city, state
+    FROM proj_stg;
+
+INSERT INTO Salary(salary)
+    SELECT DISTINCT salary
+    FROM proj_stg;
+
+INSERT INTO JobMapping(emp_id, job_id, salary_id, department_id, 
+    officelocation_id, start_date, end_date, manager_id)
+    SELECT e.employee_id, j.job_id, s.salary_id, d.department_id, 
+            o.officelocation_id, proj.start_dt, proj.end_dt,
+           (SELECT employee_id from Employee WHERE emp_name = proj.manager) AS manager_id
+    FROM proj_stg as proj
+    JOIN Employee as e ON proj.Emp_ID = e.employee_id
+    JOIN Job as j ON proj.job_title = j.jobtitle
+    JOIN Department as d ON proj.Department_nm = d.department
+    JOIN Salary as s ON proj.salary = s.salary
+    JOIN OfficeLocation AS o ON proj.location = o.location_name;
+
+
+
